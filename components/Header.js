204ce5c797042d588a72204c26a4033ddea77663
Sagara <sagara@ideabits.se>
2020-11-06 18:21:21 +0530

    first commit

---
+++ components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View, Text, StyleSheet, Platform } from 'react-native';
+
+import TitleText from './TitleText';
+import Colors from '../constants/colors';
+
+const Header = props => {
+  return (
+    <View style={styles.header}>
+      <TitleText>{props.title}</TitleText>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    width: '100%',
+    height: 90,
+    paddingTop: 36,
+    backgroundColor: Platform.OS === 'android' ?  Colors.primary : 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomColor:Platform.OS === 'ios' ? '#ccc' : 'transparent',
+  }
+});
+
+export default Header;
